053e5d34c915353fc9d681b8222fcef39245c7ac
richard483 <richard.william483@gmail.com>
2023-02-11 04:11:07 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Yae Miko" src="./assets/103255278_p0.jpg">
+  <WelcomePage msg="Welcome, dear Yae worshiper!"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WelcomePage from './components/WelcomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    WelcomePage
+}
 }
 </script>
 
@@ -21,6 +21,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 2rem;
+}
+
+img {
+  height: 32rem;
 }
 </style>
---
+++ src/components/WelcomePage.vue
@@ -0,0 +1,23 @@
+<template>
+      <h1>{{ msg }}</h1>
+      <p>
+        Yang punya web masih sibuk ngebuild Yae, jadi belum sempat bikin webnya. Tapi kalo mau liat-liat yg lain, bisa ke <a href="https://www.nephren.xyz">website utama</a> nya.
+      </p>
+  </template>
+  
+  <script>
+  export default {
+    name: 'WelcomePage',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  a {
+    color: #FFB7C6;
+  }
+  </style>
+  
